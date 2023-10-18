bdb3963a8f52f6457713ece4098ffe18fc092390
Licu Mihai <licu.mihai21@gmail.com>
2023-10-19 01:42:24 +0300

    lab CAVA si PS

---
+++ An4/Sem1/CAVA/lab/Laborator 2+3/Tema 1/cod/add_pieces_mosaic.py
@@ -0,0 +1,74 @@
+from parameters import *
+import numpy as np
+import pdb
+import timeit
+
+
+def get_mean_color_small_images(params: Parameters):
+    N, H, W, C = params.small_images.shape
+    mean_color_pieces = np.zeros((N, C))
+    for i in range(N):
+        current_image = params.small_images[i].copy()
+        for ch in range(C):
+            mean_color_pieces[i, ch] = np.float32(current_image[:, :, ch].mean())
+    return mean_color_pieces
+
+
+def get_sorted_distances(mean_color_patch, mean_color_pieces):
+    dist = np.sum((mean_color_patch - mean_color_pieces) ** 2, axis=1)
+    return np.argsort(dist)
+
+
+def add_pieces_grid(params: Parameters):
+    start_time = timeit.default_timer()
+    img_mosaic = np.zeros(params.image_resized.shape, np.uint8)
+    N, H, W, C = params.small_images.shape
+    h, w, c = params.image_resized.shape
+    num_pieces = params.num_pieces_vertical * params.num_pieces_horizontal
+
+    if params.criterion == "aleator":
+        for i in range(params.num_pieces_vertical):
+            for j in range(params.num_pieces_horizontal):
+                index = np.random.randint(low=0, high=N, size=1)
+                img_mosaic[
+                    i * H : (i + 1) * H, j * W : (j + 1) * W, :
+                ] = params.small_images[index]
+        # pass
+        # print('Building mosaic %.2f%%' % (100 * (i * params.num_pieces_horizontal + j + 1) / num_pieces))
+
+    elif params.criterion == "distantaCuloareMedie":
+        mean_color_pieces = get_mean_color_small_images(params)
+        for i in range(params.num_pieces_vertical):
+            for j in range(params.num_pieces_horizontal):
+                patch = params.image_resized[i * H : (i + 1) * H, j * W : (j + 1) * W, :]
+                mean_color_patch = np.mean(patch, axis=(0, 1))
+                sorted_indices = get_sorted_distances(mean_color_patch, mean_color_pieces)
+                index = sorted_indices[0]
+                img_mosaic[
+                    i * H : (i + 1) * H, j * W : (j + 1) * W, :
+                ] = params.small_images[index]
+
+        # pass
+        # print('Building mosaic %.2f%%' % (100 * (i * params.num_pieces_horizontal + j) / num_pieces))
+    else:
+        print("Error! unknown option %s" % params.criterion)
+        exit(-1)
+
+    end_time = timeit.default_timer()
+    print("Running time: %f s." % (end_time - start_time))
+
+    return img_mosaic
+
+
+def add_pieces_random(params: Parameters):
+    start_time = timeit.default_timer()
+    end_time = timeit.default_timer()
+    print("running time:", (end_time - start_time), "s")
+    return None
+
+
+def add_pieces_hexagon(params: Parameters):
+    start_time = timeit.default_timer()
+    end_time = timeit.default_timer()
+    print(f"running time: {end_time - start_time} s")
+    return None
---
+++ An4/Sem1/CAVA/lab/Laborator 2+3/Tema 1/cod/build_mosaic.py
@@ -0,0 +1,80 @@
+import os
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+from add_pieces_mosaic import *
+from parameters import *
+
+
+def load_pieces(params: Parameters):
+    # citeste toate cele N piese folosite la mozaic din directorul corespunzator
+    # toate cele N imagini au aceeasi dimensiune H x W x C, unde:
+    # H = inaltime, W = latime, C = nr canale (C=1  gri, C=3 color)
+    # functia intoarce pieseMozaic = matrice N x H x W x C in params
+    # pieseMoziac[i, :, :, :] reprezinta piesa numarul i
+
+    # citeste imaginile din director
+    files = os.listdir(params.small_images_dir)
+    images = [
+        cv.imread(os.path.join(params.small_images_dir, file))
+        for file in files
+        if file.endswith(params.image_type)
+    ]
+    if params.show_small_images:
+        for i in range(10):
+            for j in range(10):
+                plt.subplot(10, 10, i * 10 + j + 1)
+                # OpenCV reads images in BGR format, matplotlib reads images in RBG format
+                im = images[i * 10 + j].copy()
+                # BGR to RGB, swap the channels
+                im = im[:, :, [2, 1, 0]]
+                plt.imshow(im)
+        plt.show()
+
+    params.small_images = np.array(images)
+
+
+def compute_dimensions(params: Parameters):
+    # calculeaza dimensiunile mozaicului
+    # obtine si imaginea de referinta redimensionata avand aceleasi dimensiuni
+    # ca mozaicul
+
+    # completati codul
+    # TODO: sterge
+    hs, ws, _ = params.small_images[0].shape
+    h, w, _ = params.image.shape
+
+    # calculeaza automat numarul de piese pe verticala
+    params.num_pieces_vertical = int(
+        np.round((h * params.num_pieces_horizontal * ws) / (w * hs))
+    )
+    # redimensioneaza imaginea
+    params.image_resized = cv.resize(
+        params.image, (ws * params.num_pieces_horizontal, hs * params.num_pieces_vertical)
+    )
+
+    print(np.shape(params.image_resized))
+    print(params.num_pieces_vertical)
+
+
+def build_mosaic(params: Parameters):
+    # incarcam imaginile din care vom forma mozaicul
+    load_pieces(params)
+    # calculeaza dimensiunea mozaicului
+    compute_dimensions(params)
+
+    img_mosaic = None
+    if params.layout == "caroiaj":
+        if params.hexagon is True:
+            img_mosaic = add_pieces_hexagon(params)
+        else:
+            img_mosaic = add_pieces_grid(params)
+    elif params.layout == "aleator":
+        img_mosaic = add_pieces_random(params)
+    else:
+        print("Wrong option!")
+        exit(-1)
+
+    return img_mosaic
